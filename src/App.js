38344bec1ecae5893df09e21cc10165dbc2a42f0
sawkyawwalar <sawkyawwalarhtwe123@gmail.com>
2021-01-21 15:53:58 +0630

    revamp theme

--- src/App.js
+++ src/App.js
@@ -1,38 +1,34 @@
-import bg from './background.png'
+import bg from './images/bgImg.gif'
 import logo from './logo.svg'
-import slide2 from './images/gg.png'
-import route from "react-router-dom"
 import './App.css';
 import './tailwind.min.css'
 function App() {
   return (
-    <div className="bg-fixed bg-cover h-screen" style={{ backgroundImage: `url(${bg})` }} >
-      <div className="w-full flex border border-gray-300 rounded-lg text-gray-900">
-      <ul className="flex flex-auto">
-      <li className="text-2xl p-1">Guru</li>
-      <li className="text-xl p-3">Logo</li>
-      </ul>
-      <ul className="flex flex-1">
-      <li className="text-xl p-3 hover:text-blue-700 cursor-pointer">News</li>
-      <li className="text-xl p-3 hover:text-blue-700 cursor-pointer">Projects and Thesis</li>
-      <li className="text-xl p-3 hover:text-blue-700 cursor-pointer">Shopping and Services</li>
-      <li className="text-xl p-3 hover:text-blue-700 cursor-pointer">About Us</li>
-      </ul>
+    <div className=" font-sans bg-cover h-screen mx-auto" style={{ backgroundImage: `url(${bg})` }} >
+      <div className="w-full flex text-gray-900 border-red-700 ">
+        <ul className="flex-1">
+          <li className="text-2xl p-2 ">Guru</li>
+          <li>TechCorner</li>
+        </ul>
+        <ul className="flex flex-1 my-5">
+        <li className="text-lg px-3 py-3 text-blue-700 cursor-pointer border-b shadow-sm border-purple-300">Home</li>
+          <li className="text-lg px-3 py-3 hover:text-blue-700 cursor-pointer">News</li>
+          <li className="text-lg px-3 py-3 hover:text-blue-700 cursor-pointer">Projects and Thesis</li>
+          <li className="text-lg px-3 py-3 hover:text-blue-700 cursor-pointer">Shopping and Services</li>
+          <li className="text-lg px-3 py-3 hover:text-blue-700 cursor-pointer">About Us</li>
+        </ul>
       </div>
       <div className="flex">
-      	<div className="container m-16 px-32">
-      		<p className="container w-28">
-      			<h1 className="text-xl">Guru Tech Corner</h1>
-      			<p>One-step resource center</p>
-      		</p>
-      		<div className="container w-28 shadow-lg">
-      		<img src={logo} className="w-full h-full"/>
-      		</div>
-      	</div>
-      		
-      	<div className="container mx-16 px-32 border border-gray-600">
-      	<img src={slide2} className="w-full h-full opacity-70"/>
-      	</div>
+        <div className="container my-48 pt-32">
+          <button className="text-white bg-pink-500 border p-2 rounded-full shadow-lg">Explore More </button>
+        </div>
+        <div className="container flex-auto">
+          <div className=" border-blue-700 p-3 shadow-xl mx-auto w-2/5 my-4">
+          <span> This is the title</span><br></br>
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          </div>
+
+        </div>
       </div>
     </div>
 
